7c39597647880edeaf35d3f2bc1259c24f4dad2a
Comradewall <70492440+Comradewall@users.noreply.github.com>
2023-03-03 17:04:03 +0200

    Improved efficiency and stability

--- Line_follower_code/Line_follower_code.ino
+++ Line_follower_code/Line_follower_code.ino
@@ -9,10 +9,11 @@ int MOTOR1_PIN1 = 9;
 int MOTOR1_PIN2 = 6;
 int vitezaMS;
 int vitezaMD;
-int viteza_croaziera=70;  // se creste pana la maxim 255;
-int poz_ref=35;   //Poz ref a sens fata de linie and shit tm
-int Kp=3;   //se poate modifica  (acceleratie) valoare prea mare duce la instabilitate
-int Kd=20;    //se poate modifica   (frana/redresare) valoare prea mare duce la instabilitate
+
+int viteza_croaziera=225;      // se creste pana la maxim 255;
+int poz_ref=35;                //Poz ref a sens fata de linie and shit tm (Offset left right 35-center)
+int Kp=5;                      //se poate modifica  (acceleratie) valoare prea mare duce la instabilitate
+int Kd=20;                     //se poate modifica   (frana/redresare) valoare prea mare duce la instabilitate
 int eroare = 0;
 int eroare_ant = 0;
 int derivativ = 0;
@@ -25,41 +26,36 @@ void setup()
   pinMode(MOTOR1_PIN2, OUTPUT);
   pinMode(MOTOR2_PIN1, OUTPUT);
   pinMode(MOTOR2_PIN2, OUTPUT);
+  
   // configure the sensors
+  
   qtr.setTypeAnalog();
-  qtr.setSensorPins((const uint8_t[]){A0, A1, A2, A3, A4, A5,A6,A7}, SensorCount);
+  qtr.setSensorPins((const uint8_t[]){A0, A1, A2, A3, A4, A5, A6, A7}, SensorCount);
   qtr.setEmitterPin(2);
 
   delay(500);
 
-  autoCalibration(40,5000,10); //Function depracated in latest ver bc stability concerns
+  Calibration(250);
 }
 
 void loop() {
-  uint16_t position = qtr.readLineBlack(sensorValues)/100;
-  eroare=poz_ref-position;
-  derivativ=eroare-eroare_ant;
-  comanda=Kp*eroare+Kd*derivativ;
-  vitezaMS=viteza_croaziera-comanda;
-  vitezaMD=viteza_croaziera+comanda;
+    uint16_t position = qtr.readLineBlack(sensorValues)/100;
+  
+    eroare=poz_ref-position;
+    derivativ=eroare-eroare_ant;
+    comanda=Kp*eroare+Kd*derivativ;
+  
+    vitezaMS=viteza_croaziera-comanda;
+    vitezaMD=viteza_croaziera+comanda;
 
-  if(vitezaMS>=255)
-  {vitezaMS=255;}
+    vitezaMS = (vitezaMS > 255) ? 255 : ((vitezaMS < -255) ? -255 : vitezaMS);
+    vitezaMD = (vitezaMD > 255) ? 255 : ((vitezaMD < -255) ? -255 : vitezaMD);
 
-  if(vitezaMD>=255)
-  {vitezaMD=255;}
-
-  if(vitezaMS<=-255)
-  {vitezaMS=-255;}
-
-  if(vitezaMD<=-255)
-  {vitezaMD=-255;}
-
- go(vitezaMS,vitezaMD);
-   eroare_ant=eroare;
+    go(vitezaMS,vitezaMD);
+    eroare_ant=eroare;
 }
 
-
+//Function go should be improved in later versions when I get to it tm.
 
 void go(int speedLeft, int speedRight) {
   if (speedLeft > 0) {
@@ -80,20 +76,14 @@ void go(int speedLeft, int speedRight) {
   }
 }
 
-void autoCalibration(int speed, int ddelay, int passes) {
-
-  //Idiot profing remove as needed
-  if(speed >= 255)
-    {speed = 255;}
+void Calibration(int passes) {
   
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH); // Cal. On LED
 
   for (uint16_t i = 0; i < passes; i++)
-  {
-    go(-speed,speed); 
+  { 
     qtr.calibrate();
-    delay(ddelay);
   }
 
   digitalWrite(LED_BUILTIN, LOW); // Cal. Off LED
